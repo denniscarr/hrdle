90e27e59453821afffd4d2f2bb1177eb45033903
denniscarr <dcarr1@gmail.com>
2026-07-21 23:45:03 -0400

    Maybe a fix for loading progress always being 0

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import { compression } from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig(() => {
@@ -10,7 +11,16 @@ export default defineConfig(() => {
         "@": path.resolve(__dirname, "src"),
       },
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      // Pre-compress Godot files so we can read their size for the loading
+      // screen progress bar.
+      // Might not be necessary if we switch off Github Pages.
+      compression({
+        include: /\.(pck|wasm|js)$/,
+        deleteOriginalAssets: false,
+      }),
+    ],
     base: `/hrdle/`,
     build: {
       sourcemap: true,
